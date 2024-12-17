505b50aeb9e788cd21c7a7687a5ebc601de5655d
Yixuan_Spiked <56587752+YixuanRezZhang@users.noreply.github.com>
2024-12-17 10:35:22 +0100

    Update evaluation.py to add reg2cls

--- src/evaluation.py
+++ src/evaluation.py
@@ -100,6 +100,8 @@ class ModelEvaluator:
             if model_name == 'GaussianProcess':
                 X_tr, X_te, y_tr, y_te = train_test_split(X_bs, y_bs, test_size=0.8)
                 model = SurrogateModel(model_name, optimized_params)
+                if cls:
+                    model = RegressionToClassificationWrapper(model)
                 model.fit(X_tr, y_tr)
                 preds = model.predict(X_te)
                 
@@ -132,6 +134,8 @@ class ModelEvaluator:
     def _train_model(self, model_name, optimized_params, X_bs, y_bs, bootstrap_indices, cls, use_full_eval):
 
         model = SurrogateModel(model_name, optimized_params)
+        if cls:
+            model = RegressionToClassificationWrapper(model)
         model.fit(X_bs[bootstrap_indices], y_bs[bootstrap_indices])
 
         if use_full_eval:
